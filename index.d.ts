a82ae62ebadcbae67f5df147d14f4c6d6df714aa
spadesk1991@gmail.com <spadesk1991@gmail.com>
2019-12-18 14:16:48 +0800

    feat: change version

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,20 @@
 import { Schema } from 'mongoose'
 declare module 'egg' {
-    interface pluginOptions {
-        model: String,
-        field?: String, // default: _id
-        startAt?: number, // default: 0
-        incrementBy?: number, // default: 1
-        prefix?: String |Function |AsyncFunction // default: ""
+    interface PluginOptions {
+        model: String;
+        field?: String; // default: _id
+        startAt?: number; // default: 0
+        incrementBy?: number; // default: 1
+        // prefix?: String |Function | Promise<void>, // default: ""
         /**
          * padStart maxLength
          * eg: padStartMaxLength= 5 ---> 00005
          * default: 0
          */
-        padStartMaxLength?: number,
-        unique?: boolean, // default: true
+        padStartMaxLength?: number;
+        unique?: boolean; // default: true
     }
   export interface Application {
-    mongooseFieldAutoIncrementPlugin(schema: Schema,options: pluginOptions ): void
+    mongooseFieldAutoIncrementPlugin(schema: Schema,options: PluginOptions ): void
   }
-};
+}
